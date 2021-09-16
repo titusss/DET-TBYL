0fd002352c173f48124dee1b32ab137c2d214aea
Xiaobai <1742242048@qq.com>
2021-09-16 10:11:17 -0700

    added node.js listener

--- Frontend/gaze-follower.js
+++ Frontend/gaze-follower.js
@@ -1,8 +1,8 @@
-        const eye1 = document.querySelector('.eye-left');
-        const eye2 = document.querySelector('.eye-right')
-        window.addEventListener('mousemove', (evt) => {
-            const x = -(window.innerWidth / 2 - evt.pageX) / 8;
-            const y = -(window.innerHeight / 2 - evt.pageY) / 8;
-            eye1.style.transform = `translateY(${y}px) translateX(${x}px)`;
-            eye2.style.transform = `translateY(${y}px) translateX(${x}px)`;
-        });  
+const eye1 = document.querySelector('.eye-left');
+const eye2 = document.querySelector('.eye-right')
+window.addEventListener('mousemove', (evt) => {
+    const x = -(window.innerWidth / 2 - evt.pageX) / 8;
+    const y = -(window.innerHeight / 2 - evt.pageY) / 8;
+    eye1.style.transform = `translateY(${y}px) translateX(${x}px)`;
+    eye2.style.transform = `translateY(${y}px) translateX(${x}px)`;
+});
