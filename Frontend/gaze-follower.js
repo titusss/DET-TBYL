f789ec046b8b0c05f201ed0181e3f6c6928c89ba
titusss <26855197+titusss@users.noreply.github.com>
2021-09-16 20:09:18 -0700

    Changed to PNG monster. Mouse tracking, eye blink.

--- Frontend/gaze-follower.js
+++ Frontend/gaze-follower.js
@@ -1,8 +1,10 @@
-        const eye1 = document.querySelector('.eye-left');
-        const eye2 = document.querySelector('.eye-right')
-        window.addEventListener('mousemove', (evt) => {
-            const x = -(window.innerWidth / 2 - evt.pageX) / 8;
-            const y = -(window.innerHeight / 2 - evt.pageY) / 8;
-            eye1.style.transform = `translateY(${y}px) translateX(${x}px)`;
-            eye2.style.transform = `translateY(${y}px) translateX(${x}px)`;
-        });  
+const eye1 = document.querySelector('.eyes');
+// const eye2 = document.querySelector('.eye-right')
+window.addEventListener('mousemove', (evt) => {
+    const x = (evt.pageX - (window.innerWidth / 2)) / 110;
+    const y = 50 + (evt.pageY - (window.innerHeight / 2)) / 35;
+    console.log(y);
+    eye1.style.top = `${y}%`
+    eye1.style.left = `${x}%`;
+    // eye2.style.transform = `translateY(${y}px) translateX(${x}px)`;
+});
