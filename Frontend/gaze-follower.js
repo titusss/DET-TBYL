13128498003a85fd4251db65109679eb7480c5ee
titusss <26855197+titusss@users.noreply.github.com>
2021-09-16 20:10:51 -0700

    Changed to PNG monster. Mouse tracking, eye blink.

--- Frontend/gaze-follower.js
+++ Frontend/gaze-follower.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const eye1 = document.querySelector('.eyes');
 // const eye2 = document.querySelector('.eye-right')
 window.addEventListener('mousemove', (evt) => {
@@ -7,4 +8,13 @@ window.addEventListener('mousemove', (evt) => {
     eye1.style.top = `${y}%`
     eye1.style.left = `${x}%`;
     // eye2.style.transform = `translateY(${y}px) translateX(${x}px)`;
+=======
+const eye1 = document.querySelector('.eye-left');
+const eye2 = document.querySelector('.eye-right')
+window.addEventListener('mousemove', (evt) => {
+    const x = -(window.innerWidth / 2 - evt.pageX) / 8;
+    const y = -(window.innerHeight / 2 - evt.pageY) / 8;
+    eye1.style.transform = `translateY(${y}px) translateX(${x}px)`;
+    eye2.style.transform = `translateY(${y}px) translateX(${x}px)`;
+>>>>>>> cfa6a7ffaf21bfda5a4576d4862d81c27b1d705e
 });
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# DET-TBYL
+# Counter for Babies
+### Team: Titus, Xiaobai, Yiying, Luna
+This is the codebase of the **Counter for Babies** project.
