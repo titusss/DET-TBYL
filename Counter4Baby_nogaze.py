fb98d26bdadd86242c33ba1335f6c0f55fe46f5d
titusss <26855197+titusss@users.noreply.github.com>
2021-09-21 17:48:13 -0700

    Added experimental busy flag

--- Counter4Baby_nogaze.py
+++ Counter4Baby_nogaze.py
@@ -10,6 +10,8 @@ import socket
 import subprocess
 import sys
 
+busy = False
+
 os.system('cls||clear')
 
 gaze = GazeTracking()
@@ -47,77 +49,81 @@ def translator(str):
     return str
 
 while True:
-    print(time.time())
-    _, frame = webcam.read()
-    client = vision.ImageAnnotatorClient()
+    if busy == False:
+        print(time.time())
+        _, frame = webcam.read()
+        busy = True
+        client = vision.ImageAnnotatorClient()
 
-    # cv2.putText(new_frame, text, (60, 60), cv2.FONT_HERSHEY_DUPLEX, 2, (255, 0, 0), 2)
-    # cv2.imshow("Demo", new_frame)
+        # cv2.putText(new_frame, text, (60, 60), cv2.FONT_HERSHEY_DUPLEX, 2, (255, 0, 0), 2)
+        # cv2.imshow("Demo", new_frame)
 
-    image_bytes = cv2.imencode('.jpg', frame)[1].tobytes()
+        image_bytes = cv2.imencode('.jpg', frame)[1].tobytes()
 
-    image = vision.Image(content=image_bytes)
+        image = vision.Image(content=image_bytes)
 
-    response = client.face_detection(image=image)
-    faces = response.face_annotations
+        response = client.face_detection(image=image)
+        faces = response.face_annotations
 
-    likelihood_name = ('UNKNOWN', 'VERY_UNLIKELY', 'UNLIKELY', 'POSSIBLE',
-                       'LIKELY', 'VERY_LIKELY')
+        likelihood_name = ('UNKNOWN', 'VERY_UNLIKELY', 'UNLIKELY', 'POSSIBLE',
+                        'LIKELY', 'VERY_LIKELY')
 
-    anger_likelihood = "N"
-    joy_likelihood = "N"
-    surprise_likelihood = "N"
-    sorrow_likelihood = "N"
-    
-    detected = False
-    max_key = "sleepy"
-
-    for face in faces:
-        print('anger: {}'.format(likelihood_name[face.anger_likelihood]))
-        print('joy: {}'.format(likelihood_name[face.joy_likelihood]))
-        print('surprise: {}'.format(likelihood_name[face.surprise_likelihood]))
-        print('sorrow: {}'.format(likelihood_name[face.sorrow_likelihood]))
-        # print('blurred: {}'.format(likelihood_name[face.blurred_likelihood]))
-        # print('headwear: {}'.format(likelihood_name[face.headwear_likelihood]))
-
-        anger_likelihood = likelihood_name[face.anger_likelihood]
-        joy_likelihood = likelihood_name[face.joy_likelihood]
-        surprise_likelihood = likelihood_name[face.surprise_likelihood]
-        sorrow_likelihood = likelihood_name[face.sorrow_likelihood]
+        anger_likelihood = "N"
+        joy_likelihood = "N"
+        surprise_likelihood = "N"
+        sorrow_likelihood = "N"
         
-        detected = True
-
-        # vertices = (['({},{})'.format(vertex.x, vertex.y)
-        #             for vertex in face.bounding_poly.vertices])
-
-        # print('face bounds: {}'.format(','.join(vertices)))
-
-    if response.error.message:
-        raise Exception(
-            '{}\nFor more info on error messages, check: '
-            'https://cloud.google.com/apis/design/errors'.format(
-                response.error.message))
-
-    dic = {"happy":translator(joy_likelihood),"sad":translator(sorrow_likelihood), "sleepy":translator(anger_likelihood)}
-
-    if detected==False:
+        detected = False
         max_key = "sleepy"
-    elif translator(joy_likelihood)<1 and translator(sorrow_likelihood)<1 and translator(anger_likelihood)<1:
-        max_key = "neutral"
-    else:
-        max_key = max(dic, key=dic.get)
 
-    # print(max_key)
-    detected = False
-    
-    
-    text2 = str(0.5)+ ";"+str(0.5)+ ";"+max_key
-    print(text2)
-    MESSAGE = text2.encode()
-    sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+        for face in faces:
+            print('anger: {}'.format(likelihood_name[face.anger_likelihood]))
+            print('joy: {}'.format(likelihood_name[face.joy_likelihood]))
+            print('surprise: {}'.format(likelihood_name[face.surprise_likelihood]))
+            print('sorrow: {}'.format(likelihood_name[face.sorrow_likelihood]))
+            # print('blurred: {}'.format(likelihood_name[face.blurred_likelihood]))
+            # print('headwear: {}'.format(likelihood_name[face.headwear_likelihood]))
 
-    #Recording time to manage performance
-    print(time.time())
+            anger_likelihood = likelihood_name[face.anger_likelihood]
+            joy_likelihood = likelihood_name[face.joy_likelihood]
+            surprise_likelihood = likelihood_name[face.surprise_likelihood]
+            sorrow_likelihood = likelihood_name[face.sorrow_likelihood]
+            
+            detected = True
+
+            # vertices = (['({},{})'.format(vertex.x, vertex.y)
+            #             for vertex in face.bounding_poly.vertices])
+
+            # print('face bounds: {}'.format(','.join(vertices)))
+
+        if response.error.message:
+            busy = False
+            raise Exception(
+                '{}\nFor more info on error messages, check: '
+                'https://cloud.google.com/apis/design/errors'.format(
+                    response.error.message))
+
+        dic = {"happy":translator(joy_likelihood),"sad":translator(sorrow_likelihood), "sleepy":translator(anger_likelihood)}
+
+        if detected==False:
+            max_key = "sleepy"
+        elif translator(joy_likelihood)<1 and translator(sorrow_likelihood)<1 and translator(anger_likelihood)<1:
+            max_key = "neutral"
+        else:
+            max_key = max(dic, key=dic.get)
+
+        # print(max_key)
+        detected = False
+        
+        
+        text2 = str(0.5)+ ";"+str(0.5)+ ";"+max_key
+        print(text2)
+        MESSAGE = text2.encode()
+        sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+        
+        busy = False
+        #Recording time to manage performance
+        print(time.time())
 
     if cv2.waitKey(1) == 27:
         break
